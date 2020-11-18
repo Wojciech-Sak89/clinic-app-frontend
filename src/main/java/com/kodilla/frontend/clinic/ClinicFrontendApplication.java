e3216d29156e0a91b23efe22eb8f9e5ea00e0415
Wojciech-Sak89 <wojciech.r.sak@gmail.com>
2020-11-18 13:24:46 +0100

    Vaddin working setup - Hellow world

--- src/main/java/com/kodilla/frontend/clinic/ClinicFrontendApplication.java
+++ src/main/java/com/kodilla/frontend/clinic/ClinicFrontendApplication.java
@@ -1,8 +1,10 @@
 package com.kodilla.frontend.clinic;
 
+import com.vaadin.flow.spring.annotation.EnableVaadin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableVaadin
 @SpringBootApplication
 public class ClinicFrontendApplication {
 
